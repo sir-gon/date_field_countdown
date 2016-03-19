00e4bacd5dd80b5d3aca0dea8e4134e94ea9a90e
Gonzalo Diaz <devel@gon.cl>
2016-03-19 13:01:11 -0300

    First attempt to render javascript counter, with static values.

--- date_field_cowntdown.tpl.php
+++ date_field_cowntdown.tpl.php
@@ -1,1 +1,54 @@
-<div class="date-field-countdown"><?php print $value; ?></div> 
+<?php
+$key = md5(uniqid(rand(), true));
+$key = substr($key, 0, 5);
+
+$field_id = 'date-field-countdown-'.$key;
+$field_id_note = $field_id.'-note';
+$field_id_countdown = $field_id.'-countdown';
+?>
+
+
+<div class="date-field-countdown">
+	<!-- <?php echo $value; ?> -->
+	<div id="<?php echo $field_id_countdown; ?>"></div>
+	<p id="<?php echo $field_id_note; ?>"></p>
+</div> 
+
+<script type="text/javascript">
+(function($){
+
+    var note = $('#<?php echo $field_id_note; ?>'),
+        ts = new Date(2012, 0, 1),
+        newYear = true;
+
+    if((new Date()) > ts){
+        // The new year is here! Count towards something else.
+        // Notice the *1000 at the end - time must be in milliseconds
+        ts = (new Date()).getTime() + 10*24*60*60*1000;
+        newYear = false;
+    }
+
+    $('#<?php echo $field_id_countdown; ?>').countdown({
+        timestamp   : ts,
+        callback    : function(days, hours, minutes, seconds){
+
+            var message = "";
+
+            message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+            message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+            message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+            message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+
+            if(newYear){
+                message += "left until the new year!";
+            }
+            else {
+                message += "left to 10 days from now!";
+            }
+
+            note.html(message);
+        }
+    });
+
+})(jQuery);
+</script>
